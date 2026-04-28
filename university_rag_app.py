812b4108f74229f57202f6dbb992ee82b3844f16
amadan1-ai <amadan1@uchicago.edu>
2026-04-27 23:39:43 -0400

    Update university_rag_app.py

--- university_rag_app.py
+++ university_rag_app.py
@@ -495,7 +495,7 @@ with st.sidebar:
     st.markdown('<div class="sidebar-label">Vector Store Path</div>', unsafe_allow_html=True)
     persist_dir = st.text_input(
         "Vector Store Path",
-        value= "./chroma_university_kb"",
+        value= "./chroma_university_kb",
         label_visibility="collapsed",
     )
 
